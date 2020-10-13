afd35807c13d08253f11aae2c0288976b6971b4b
imran-shaikh-au6 <imranshaikh110120@gmail.com>
2020-10-13 11:30:55 +0530

    m

--- src/client/components/ProfileComponent.js
+++ src/client/components/ProfileComponent.js
@@ -78,25 +78,40 @@ class ProfileComponent extends React.Component {
                             </span>
                         </p>
                         <p className={"dashboard-profile-deatils"}>
-                            Hello, my name is Imran Shaikh, I am a certified
-                            UX/UI Web designer and a Full Stack Web Developer I
-                            have extensive experience in creating modern and
-                            responsive Web Application from scratch and in
-                            development of Enterprise Web application. I breathe
-                            design and creativity.I am also passionate about
-                            coding too. In terms of the Web framework having
-                            experience in both back-end and front-end
-                            development, developed many full-stack based web
-                            application using Node.js, React.js & MongoDB.
+                            Hello My Name is Imran Shaikh. I am a MERN Stack
+                            developer for about 1.2 years ranging from small scale
+                            websites to large scale eCommerce web applications.
+                            (Stripe, Paypal, payment gateway integrations !). I
+                            breathe coding, I am also passionate about designing
+                            too. In terms of the Web framework having experience
+                            in both back-end and front-end development,
+                            developed many full-stack based web application
+                            using Node.js, React.js & MongoDB, PostgresSQL.
+                        </p>
+                        <p className={"dashboard-profile-deatils"}>
+                            It all started in 2019 where I first learnt to build
+                            static landing pages. Then I came to know about how
+                            servers work with the help of Node.JS, Python. That
+                            expanded my view upon the possibilities of creating
+                            huge applications
+                        </p>
+                        <p className={"dashboard-profile-deatils"}>
+                            Then I slowly started engaging myself building
+                            single page application development, state
+                            management techniques using Redux.js and Context API
+                            and sideeffects using Redux-thunk and Redux-Saga,
+                            building microservices in Node.js and Express.js and
+                            knowing how to connect to databases using MongoDB
+                            and MySQL.
                         </p>
                         <p className={"dashboard-profile-deatils"}>
                             I always have been fascinated between the marriage
                             of design and programming .I have worked alongside
                             some talented teams on a number of high profile
                             projects. I have a wide range of skills that include
-                            design front-end development with HTML5, CSS3,
-                            Javascript, Responsive web design and back-end
-                            development .
+                            design front-end development with React.JS, HTML5,
+                            CSS3, MaterialUI Javascript, Responsive web design
+                            and back-end development.
                         </p>
                     </div>
                 </div>
